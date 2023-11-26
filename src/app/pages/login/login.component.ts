2157d9e92ce3fe4704e116c555e04eb611f23c6e
MiguelCardona11 <04.migue.cardona@gmail.commit>
2023-11-25 19:39:10 -0500

    funcionamiento lógico del login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { SecurityService } from 'src/app/services/security.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +11,85 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+
+  formGroupValidator: FormGroup;
+  constructor(private formBuilder: FormBuilder,
+              private router:Router,
+              private securityService:SecurityService) {}
 
   ngOnInit() {
+    this.formBuilding()
   }
+  formBuilding(){
+    this.formGroupValidator=this.formBuilder.group({
+      email : ['', [Validators.required, Validators.email]],
+      password : ['', [Validators.required, Validators.minLength(2)]],
+    });
+  }
+
+  login() {
+
+    if(this.formGroupValidator.invalid) {
+      Swal.fire({
+        title: 'Formulario Incorrecto',
+        icon: 'error',
+        timer: 3000
+      });
+      return false;
+    }
+    let theUser=this.userData()
+    this.securityService.login(theUser).subscribe({
+      next: (data) => {
+        console.log("llamando")
+        console.log("resultado login "+ data)
+        this.getUserFromToken(data)
+        this.router.navigate(['pages/dashboard']);
+        // this.securityService.saveSessionData(data);
+      },
+
+      error: (error) => {
+        Swal.fire({
+          title: 'Error',
+          text: 'Usuario o contraseña inválido',
+          icon: 'error',
+          timer:5000 
+        });
+      }
+    });
+
+
+
+  }
+
+  getUserFromToken(token: string) {
+    this.securityService.getUserFromToken(token).subscribe({
+      next: dataUser =>{
+        console.log(dataUser)
+        let theUser:User={
+          "_id":dataUser["_id"],
+          "name":dataUser["name"],
+          "email":dataUser["email"],
+          "token":dataUser["token"],
+          "role":dataUser["role"],
+        }
+        this.securityService.saveSessionData(theUser);
+      }
+    })
+  }
+
+
   ngOnDestroy() {
+  }
+
+  get formGroupValidatorData(){
+    return this.formGroupValidator.controls;
+  }
+
+  userData() : User{
+    let theUser= new User();
+    theUser.email=this.formGroupValidatorData.email.value;
+    theUser.password=this.formGroupValidatorData.password.value;
+    return theUser;
   }
 
 }
